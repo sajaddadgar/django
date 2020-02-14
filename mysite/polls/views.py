4e98daee7caea68898834b955b6e8592e9691ed3
s.dadgar <sajad.dadgar98@gmail.com>
2020-02-14 20:09:05 +0330

    Add some code

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,10 @@
+from django.db.models import F
 from django.shortcuts import render, get_object_or_404
 from .models import Question, Choice
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+
 
 
 # Create your views here.
@@ -28,11 +31,24 @@ def result(request, question_id):
     stuff = {
         'question': question
     }
-    return render(request, 'polls/choice.html', stuff)
+    return render(request, 'polls/result.html', stuff)
 
 
 def vote(request, question_id):
-    return HttpResponse('you are in vote page in question: %s' % question_id)
+    # return HttpResponse('you are in vote page in question: %s' % question_id)
+    question = get_object_or_404(Question, pk =     question_id)
+    stuff_for_frontend = {
+        'question': question,
+        'error_message': "You didn't select a choice.",
+    }
+    try:
+        selectedChoice = question.choice_set.get(pk = request.POST['aaa'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', stuff_for_frontend)
+    else:
+        selectedChoice.votes += 1
+        selectedChoice.save()
+        return HttpResponseRedirect(reverse('polls:result', args=(question_id,)))
 
 def showDetail(request):
     latestQuestionList = Question.objects.order_by('-pub_date')[:5]
